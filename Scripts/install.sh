b4f884506aeb9f17f59fc9f4b02f70a52fd28cbb
Hazim Bukhari <80190998+FancyWhale69@users.noreply.github.com>
2022-11-24 16:57:25 +0300

    Fixed an issue with exit statments

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -23,11 +23,11 @@ done
 
 
 cd ..
-git clone https://github.com/zhangchenxu528/FACIAL.git || echo "$_ failed to download"; exit 1
-git clone https://github.com/sicxu/Deep3DFaceRecon_pytorch.git || echo "$_ failed to download"; exit 1
+git clone https://github.com/zhangchenxu528/FACIAL.git || (echo "$_ failed to download"; exit 1)
+git clone https://github.com/sicxu/Deep3DFaceRecon_pytorch.git || (echo "$_ failed to download"; exit 1)
 conda activate deep
-cd Deep3DFaceRecon_pytorch || echo "$_ dir does not exist"; exit 1
-git clone https://github.com/NVlabs/nvdiffrast || echo "$_ failed to download"; exit 1
+cd Deep3DFaceRecon_pytorch || (echo "$_ dir does not exist"; exit 1)
+git clone https://github.com/NVlabs/nvdiffrast || (echo "$_ failed to download"; exit 1)
 
 if [ $nvd -eq 0 ]
 then
@@ -37,14 +37,14 @@ cd ..
 fi
 
 
-git clone https://github.com/deepinsight/insightface.git || echo "$_ failed to download"; exit 1
+git clone https://github.com/deepinsight/insightface.git || (echo "$_ failed to download"; exit 1)
 cp -r ./insightface/recognition/arcface_torch ./models/
-# cp ../models/01_MorphableModel.mat ./BFM/01_MorphableModel.mat
-# cp ../models/Exp_Pca.bin ./BFM/Exp_Pca.bin
+cp ../models/01_MorphableModel.mat ./BFM/01_MorphableModel.mat
+cp ../models/Exp_Pca.bin ./BFM/Exp_Pca.bin
 
 mkdir ./checkpoints
 mkdir ./checkpoints/model
-# cp ../models/epoch_20.pth ./checkpoints/model/epoch_20.pth
+cp ../models/epoch_20.pth ./checkpoints/model/epoch_20.pth
 
 cd ..
 
@@ -59,20 +59,20 @@ pip install gdown
 fi
 
 #https://drive.google.com/uc?id=
-gdown -O ./Deep3DFaceRecon_pytorch/BFM/01_MorphableModel.mat https://drive.google.com/uc?id=1K3Ojg5waDaYexNYkg006zpqR-BNobvRi || echo "$_ failed to download"; exit 1
-gdown -O ./Deep3DFaceRecon_pytorch/BFM/Exp_Pca.bin https://drive.google.com/uc?id=1VitGK-HEsIVyAf0LLyBZ7gYM6Pz1H_71 || echo "$_ failed to download"; exit 1
-gdown -O ./Deep3DFaceRecon_pytorch/checkpoints/model/epoch_20.pth https://drive.google.com/uc?id=1xRZSsoPF3O3dMr3ZhRI04MLPC7pSMH0n || echo "$_ failed to download"; exit 1
+# gdown -O ./Deep3DFaceRecon_pytorch/BFM/01_MorphableModel.mat https://drive.google.com/uc?id=1K3Ojg5waDaYexNYkg006zpqR-BNobvRi || (echo "$_ failed to download"; exit 1)
+# gdown -O ./Deep3DFaceRecon_pytorch/BFM/Exp_Pca.bin https://drive.google.com/uc?id=1VitGK-HEsIVyAf0LLyBZ7gYM6Pz1H_71 || (echo "$_ failed to download"; exit 1)
+# gdown -O ./Deep3DFaceRecon_pytorch/checkpoints/model/epoch_20.pth https://drive.google.com/uc?id=1xRZSsoPF3O3dMr3ZhRI04MLPC7pSMH0n || (echo "$_ failed to download"; exit 1)
 
-gdown -O ./FACIAL/audio2face/checkpoint/obama/Gen-20-0.0006273046686902202.mdl https://drive.google.com/uc?id=1gA14zQZm3bOxSNkbuYdBkcDNxrX1POJv || echo "$_ failed to download"; exit 1
-gdown -O ./FACIAL/audio2face/ds_graph/output_graph.pb https://drive.google.com/uc?id=183hTRI96jIoFehSH79LUhFIlnao8mVmT || echo "$_ failed to download"; exit 1
-gdown -O ./FACIAL/face_render/BFM/BFM_model_front.mat https://drive.google.com/uc?id=1FJy1AkDUHY5QsiOA0alc90czw-XiikJ7 || echo "$_ failed to download"; exit 1
-gdown -O ./FACIAL/face_render/BFM/std_exp.txt https://drive.google.com/uc?id=1BKi_oTDoNaHpt_sbkpgO1CwvbEMTLCH7 || echo "$_ failed to download"; exit 1
+# gdown -O ./FACIAL/audio2face/checkpoint/obama/Gen-20-0.0006273046686902202.mdl https://drive.google.com/uc?id=1gA14zQZm3bOxSNkbuYdBkcDNxrX1POJv || (echo "$_ failed to download"; exit 1)
+# gdown -O ./FACIAL/audio2face/ds_graph/output_graph.pb https://drive.google.com/uc?id=183hTRI96jIoFehSH79LUhFIlnao8mVmT || (echo "$_ failed to download"; exit 1)
+# gdown -O ./FACIAL/face_render/BFM/BFM_model_front.mat https://drive.google.com/uc?id=1FJy1AkDUHY5QsiOA0alc90czw-XiikJ7 || (echo "$_ failed to download"; exit 1)
+# gdown -O ./FACIAL/face_render/BFM/std_exp.txt https://drive.google.com/uc?id=1BKi_oTDoNaHpt_sbkpgO1CwvbEMTLCH7 || (echo "$_ failed to download"; exit 1)
 
 
-# cp ./models/Gen-20-0.0006273046686902202.mdl ./FACIAL/audio2face/checkpoint/obama/Gen-20-0.0006273046686902202.mdl
-# cp ./models/output_graph.pb ./FACIAL/audio2face/ds_graph/output_graph.pb
-# cp ./models/BFM_model_front.mat ./FACIAL/face_render/BFM/BFM_model_front.mat
-# cp ./models/std_exp.txt ./FACIAL/face_render/BFM/std_exp.txt
+cp ./models/Gen-20-0.0006273046686902202.mdl ./FACIAL/audio2face/checkpoint/obama/Gen-20-0.0006273046686902202.mdl
+cp ./models/output_graph.pb ./FACIAL/audio2face/ds_graph/output_graph.pb
+cp ./models/BFM_model_front.mat ./FACIAL/face_render/BFM/BFM_model_front.mat
+cp ./models/std_exp.txt ./FACIAL/face_render/BFM/std_exp.txt
 
 
 cp ./Scripts/lands.py ./Deep3DFaceRecon_pytorch/lands.py
--- Scripts/preprocess.sh
+++ Scripts/preprocess.sh
@@ -18,7 +18,7 @@ do
     fi
 done
 
-ffmpeg -i ../FACIAL/video_preprocess/train1.mp4 -r 30 ../FACIAL/video_preprocess/train1_image/%d.png || echo "ffmpeg failed"; exit 1
+ffmpeg -i ../FACIAL/video_preprocess/train1.mp4 -r 30 ../FACIAL/video_preprocess/train1_image/%d.png || (echo "ffmpeg failed"; exit 1)
 
 #Docker for openface
 cd ../FACIAL/video_preprocess/train1_image || exit 1
@@ -33,23 +33,23 @@ sudo docker exec -ti "$docname" cd ..
 sudo docker exec -ti "$docname" ./build/bin/FeatureExtraction -fdir ./train1_512_audio 
 sudo docker exec -ti "$docname" cp processed/train1_512_audio.csv train1_512_audio
 sudo docker stop "$docname"
-cp ../FACIAL/video_preprocess/train1_image/train1_512_audio.csv ../FACIAL/video_preprocess/train1_openface || echo "train1_512_audio.csv did not copy"; exit 1
-rm -f ../FACIAL/video_preprocess/train1_image/train1_512_audio.csv || echo "could not remove train1_image/train1_512_audio.csv"; exit 1
+cp ../FACIAL/video_preprocess/train1_image/train1_512_audio.csv ../FACIAL/video_preprocess/train1_openface || (echo "train1_512_audio.csv did not copy"; exit 1)
+rm -f ../FACIAL/video_preprocess/train1_image/train1_512_audio.csv || (echo "could not remove train1_image/train1_512_audio.csv"; exit 1)
 
 #get 5-point landmarks and place detections inside train1_image
 conda activate mtcnn
 python ../Deep3DFaceRecon_pytorch/lands.py -i ../FACIAL/video_preprocess/train1_image -o ../FACIAL/video_preprocess/detections || exit 1
-cp -R ../FACIAL/video_preprocess/detections ../FACIAL/video_preprocess/train1_image/detections || echo "could not copy detections into train1_image"; exit 1
+cp -R ../FACIAL/video_preprocess/detections ../FACIAL/video_preprocess/train1_image/detections || (echo "could not copy detections into train1_image"; exit 1)
 
 #strat extracting 3d mesh of the face, copy .mat files to train1_deep3Dface, and delete detections from train1_image
 conda activate deep
 cd ../Deep3DFaceRecon_pytorch/ || exit 1
 python test.py --name=model --epoch=20 --img_folder=../FACIAL/video_preprocess/train1_image || exit 1
 cd ./checkpoints/model/results/train1_image || exit 1
-cp ./epoch_20_000000/*.mat ../../../../../FACIAL/video_preprocess/train1_deep3Dface || echo "An Error happened while coping files from Deep3DFaceRecon_pytorch/checkpoints/model/results/train1_image/epoch_20_000000" ; exit 1
+cp ./epoch_20_000000/*.mat ../../../../../FACIAL/video_preprocess/train1_deep3Dface || (echo "An Error happened while coping files from Deep3DFaceRecon_pytorch/checkpoints/model/results/train1_image/epoch_20_000000" ; exit 1)
 cd ../.. && rm -r ./results || exit 1
 cd ../../.. || exit 1
-rm -r ./FACIAL/video_preprocess/train1_image/detections || echo "could not remove detections from inside train1_image"; exit 1
+rm -r ./FACIAL/video_preprocess/train1_image/detections || (echo "could not remove detections from inside train1_image"; exit 1)
 
 
 
