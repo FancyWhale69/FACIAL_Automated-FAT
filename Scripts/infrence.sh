02c780ef27af64f4328ad3af49b5164b7179442d
Hazim Bukhari <80190998+FancyWhale69@users.noreply.github.com>
2022-11-28 14:15:32 +0300

    Add files via upload

--- Scripts/infrence.sh
+++ Scripts/infrence.sh
@@ -24,20 +24,20 @@ fi
 
 conda activate facial
 
-cd ../FACIAL || exit 1
+cd ../FACIAL  
 
-cp -r ./video_preprocess/"$def.wav" ./examples/audio/"$def.wav" || exit 1
+cp -r ./video_preprocess/"$def.wav" ./examples/audio/"$def.wav"  
 
-cd audio2face || exit 1
+cd audio2face  
 
 #if audio is already processed, skip this step
 if ! test -f ../FACIAL/examples/test-result/"$def.npz"
 then
-python audio_preprocessing.py || exit 1
-python test.py --audiopath ../examples/audio_preprocessed/"$def.pkl" --checkpath ./checkpoint/train1/Gen-10.mdl || exit 1
+python audio_preprocessing.py  
+python test.py --audiopath ../examples/audio_preprocessed/"$def.pkl" --checkpath ./checkpoint/train1/Gen-10.mdl  
 fi
 
-cd ../face_render || exit 1
+cd ../face_render  
 
 #delete old renders
 if [ -d "../examples/rendering/$def" ]
@@ -45,18 +45,18 @@ then
 rm -r "../examples/rendering/$def"
 fi
 
-python rendering_gaosi.py --train_params_path ../video_preprocess/train1_posenew.npz --net_params_path ../examples/test-result/"$def.npz" --outpath '../examples/rendering/' || exit 1
+python rendering_gaosi.py --train_params_path ../video_preprocess/train1_posenew.npz --net_params_path ../examples/test-result/"$def.npz" --outpath '../examples/rendering/'  
 
 cd ..
-rm -rf ./face2vid/datasets/train3/test_A ./face2vid/datasets/train3/test_B || exit 1
-cp -r ./examples/rendering/"$def" ./face2vid/datasets/train3/test_A || exit 1
-cp -r ./examples/rendering/"$def" ./face2vid/datasets/train3/test_B || exit 1
+rm -rf ./face2vid/datasets/train3/test_A ./face2vid/datasets/train3/test_B  
+cp -r ./examples/rendering/"$def" ./face2vid/datasets/train3/test_A  
+cp -r ./examples/rendering/"$def" ./face2vid/datasets/train3/test_B  
 
-cd face2vid || exit 1
-python test_video.py --test_id_name "$def" --blink_path "../examples/test-result/$def.npz" --name train3 --model pose2vid --dataroot ./datasets/train3/ --which_epoch "$defe" --netG local --ngf 32 --label_nc 0 --n_local_enhancers 1 --no_instance --resize_or_crop resize || exit 1
-ffmpeg -y -i "../examples/test_image/$def/test_1.avi" -i "../video_preprocess/$def.wav" -c copy "../examples/test_image/$def/test_1_audio.avi" || exit 1
-ffmpeg -y -i "../examples/test_image/$def/test_1_audio.avi"  "../examples/test_image/$def/test_1_audio.mp4" || exit 1
+cd face2vid  
+python test_video.py --test_id_name "$def" --blink_path "../examples/test-result/$def.npz" --name train3 --model pose2vid --dataroot ./datasets/train3/ --which_epoch "$defe" --netG local --ngf 32 --label_nc 0 --n_local_enhancers 1 --no_instance --resize_or_crop resize  
+ffmpeg -y -i "../examples/test_image/$def/test_1.avi" -i "../video_preprocess/$def.wav" -c copy "../examples/test_image/$def/test_1_audio.avi"  
+ffmpeg -y -i "../examples/test_image/$def/test_1_audio.avi"  "../examples/test_image/$def/test_1_audio.mp4"  
 
-cp ../examples/test_image/"$def"/test_1_audio.mp4 ../Results/"$def"_epoch_"$defe"_results.mp4 || exit 1
+cp ../examples/test_image/"$def"/test_1_audio.mp4 ../Results/"$def"_epoch_"$defe"_results.mp4  
  
 echo "Output is saved in Results folder"
--- Scripts/preprocess.sh
+++ Scripts/preprocess.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if ! test -f ../FACIAL/video_preprocess/train1.mp4 || ! test -f ../FACIAL/video_preprocess/test1.wav
+if ! test -f ../FACIAL/video_preprocess/train1.mp4  
 then 
 echo 'Training video or Test audio are not found'
 exit
@@ -18,12 +18,12 @@ do
     fi
 done
 
-ffmpeg -i ../FACIAL/video_preprocess/train1.mp4 -r 30 ../FACIAL/video_preprocess/train1_image/%d.png || (echo "ffmpeg failed"; exit 1)
+ffmpeg -i ../FACIAL/video_preprocess/train1.mp4 -r 30 ../FACIAL/video_preprocess/train1_image/%d.png  
 
 #Docker for openface
-cd ../FACIAL/video_preprocess/train1_image || exit 1
+cd ../FACIAL/video_preprocess/train1_image  
 path=$(pwd)
-cd ../../../Scripts || exit 1
+cd ../../../Scripts  
 sudo docker run -itd --name "$docname"  -v "$path":/home/openface-build/train1_512_audio --rm algebr/openface:latest 
 sudo docker exec -ti "$docname" git clone https://github.com/xianyi/OpenBLAS.git
 sudo docker exec -ti "$docname" cd OpenBLAS
@@ -33,23 +33,23 @@ sudo docker exec -ti "$docname" cd ..
 sudo docker exec -ti "$docname" ./build/bin/FeatureExtraction -fdir ./train1_512_audio 
 sudo docker exec -ti "$docname" cp processed/train1_512_audio.csv train1_512_audio
 sudo docker stop "$docname"
-cp ../FACIAL/video_preprocess/train1_image/train1_512_audio.csv ../FACIAL/video_preprocess/train1_openface || (echo "train1_512_audio.csv did not copy"; exit 1)
-rm -f ../FACIAL/video_preprocess/train1_image/train1_512_audio.csv || (echo "could not remove train1_image/train1_512_audio.csv"; exit 1)
+cp ../FACIAL/video_preprocess/train1_image/train1_512_audio.csv ../FACIAL/video_preprocess/train1_openface  
+rm -f ../FACIAL/video_preprocess/train1_image/train1_512_audio.csv  
 
 #get 5-point landmarks and place detections inside train1_image
 conda activate mtcnn
-python ../Deep3DFaceRecon_pytorch/lands.py -i ../FACIAL/video_preprocess/train1_image -o ../FACIAL/video_preprocess/detections || exit 1
-cp -R ../FACIAL/video_preprocess/detections ../FACIAL/video_preprocess/train1_image/detections || (echo "could not copy detections into train1_image"; exit 1)
+python ../Deep3DFaceRecon_pytorch/lands.py -i ../FACIAL/video_preprocess/train1_image -o ../FACIAL/video_preprocess/detections  
+cp -R ../FACIAL/video_preprocess/detections ../FACIAL/video_preprocess/train1_image/detections  
 
 #strat extracting 3d mesh of the face, copy .mat files to train1_deep3Dface, and delete detections from train1_image
 conda activate deep
-cd ../Deep3DFaceRecon_pytorch/ || exit 1
-python test.py --name=model --epoch=20 --img_folder=../FACIAL/video_preprocess/train1_image || exit 1
-cd ./checkpoints/model/results/train1_image || exit 1
-cp ./epoch_20_000000/*.mat ../../../../../FACIAL/video_preprocess/train1_deep3Dface || (echo "An Error happened while coping files from Deep3DFaceRecon_pytorch/checkpoints/model/results/train1_image/epoch_20_000000" ; exit 1)
-cd ../.. && rm -r ./results || exit 1
-cd ../../.. || exit 1
-rm -r ./FACIAL/video_preprocess/train1_image/detections || (echo "could not remove detections from inside train1_image"; exit 1)
+cd ../Deep3DFaceRecon_pytorch/  
+python test.py --name=model --epoch=20 --img_folder=../FACIAL/video_preprocess/train1_image  
+cd ./checkpoints/model/results/train1_image  
+cp ./epoch_20_000000/*.mat ../../../../../FACIAL/video_preprocess/train1_deep3Dface  
+cd ../.. && rm -r ./results  
+cd ../../..  
+rm -r ./FACIAL/video_preprocess/train1_image/detections  
 
 
 
