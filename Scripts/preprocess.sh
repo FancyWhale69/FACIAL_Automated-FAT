fa7ff65ffef3e5b8671bbe59f6bc2c7f1d65d42b
Hazim Bukhari <80190998+FancyWhale69@users.noreply.github.com>
2023-01-09 15:05:40 +0300

    Update preprocess.sh

--- Scripts/preprocess.sh
+++ Scripts/preprocess.sh
@@ -8,33 +8,33 @@ fi
 sudo echo
 
 # Docker container name
-while : 
-do
-    echo "Enter a unique name:"
-    read docname 
-    if [ ! -z "$docname" ]
-    then 
-    break
-    fi
-done
+# while : 
+# do
+#     echo "Enter a unique name:"
+#     read docname 
+#     if [ ! -z "$docname" ]
+#     then 
+#     break
+#     fi
+# done
 
 ffmpeg -i ../FACIAL/video_preprocess/train1.mp4 -r 30 ../FACIAL/video_preprocess/train1_image/%d.png  
 
 #Docker for openface
-cd ../FACIAL/video_preprocess/train1_image  
-path=$(pwd)
-cd ../../../Scripts  
-sudo docker run -itd --name "$docname"  -v "$path":/home/openface-build/train1_512_audio --rm algebr/openface:latest 
-sudo docker exec -ti "$docname" git clone https://github.com/xianyi/OpenBLAS.git
-sudo docker exec -ti "$docname" cd OpenBLAS
-sudo docker exec -ti "$docname" make -j4
-sudo docker exec -ti "$docname" make install
-sudo docker exec -ti "$docname" cd ..
-sudo docker exec -ti "$docname" ./build/bin/FeatureExtraction -fdir ./train1_512_audio 
-sudo docker exec -ti "$docname" cp processed/train1_512_audio.csv train1_512_audio
-sudo docker stop "$docname"
-cp ../FACIAL/video_preprocess/train1_image/train1_512_audio.csv ../FACIAL/video_preprocess/train1_openface  
-rm -f ../FACIAL/video_preprocess/train1_image/train1_512_audio.csv  
+# cd ../FACIAL/video_preprocess/train1_image  
+# path=$(pwd)
+# cd ../../../Scripts  
+# sudo docker run -itd --name "$docname"  -v "$path":/home/openface-build/train1_512_audio --rm algebr/openface:latest 
+# sudo docker exec -ti "$docname" git clone https://github.com/xianyi/OpenBLAS.git
+# sudo docker exec -ti "$docname" cd OpenBLAS
+# sudo docker exec -ti "$docname" make -j4
+# sudo docker exec -ti "$docname" make install
+# sudo docker exec -ti "$docname" cd ..
+# sudo docker exec -ti "$docname" ./build/bin/FeatureExtraction -fdir ./train1_512_audio 
+# sudo docker exec -ti "$docname" cp processed/train1_512_audio.csv train1_512_audio
+# sudo docker stop "$docname"
+# cp ../FACIAL/video_preprocess/train1_image/train1_512_audio.csv ../FACIAL/video_preprocess/train1_openface  
+# rm -f ../FACIAL/video_preprocess/train1_image/train1_512_audio.csv  
 
 #get 5-point landmarks and place detections inside train1_image
 conda activate mtcnn
